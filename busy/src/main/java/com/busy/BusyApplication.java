5adfd8ceff123103bd741a1b7844c99c2fcad1de
xc54677 <gesangcx@outlook.com>
2018-08-06 22:23:09 +0800

    one
    one day

---
+++ busy/src/main/java/com/busy/BusyApplication.java
@@ -0,0 +1,12 @@
+package com.busy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusyApplication.class, args);
+	}
+}
